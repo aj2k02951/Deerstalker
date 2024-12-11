7f343af8b75293a6fe0e082e8cd524ecb0c8d8c4
aj2k02951 <aj2k02@gmail.com>
2024-12-11 12:47:24 -0800

    Initial Commit

---
+++ Deerstalker/DeerstalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeerstalkerApp.swift
+//  Deerstalker
+//
+//  Created by AJ Valenzuela on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DeerstalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeerstalkerTests/DeerstalkerTests.swift
@@ -0,0 +1,16 @@
+//
+//  DeerstalkerTests.swift
+//  DeerstalkerTests
+//
+//  Created by AJ Valenzuela on 12/11/24.
+//
+
+import Testing
+
+struct DeerstalkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
